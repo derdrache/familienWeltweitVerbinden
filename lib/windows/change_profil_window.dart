554a28273c3127d14493d8f7efd344201e735466
derdrache <dominik.mast.11@gmail.com>
2022-02-05 21:48:11 -0500

    clean code

--- lib/windows/change_profil_window.dart
+++ lib/windows/change_profil_window.dart
@@ -1,59 +1,60 @@
 import 'package:flutter/material.dart';
+import '../global/style.dart' as global_style;
 
 
-_topBar(context, titel, Function saveFunction){
-  return Row(
-    children: [
-      TextButton(
-        style: ButtonStyle(
-            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20.0),
-                )
-            )
+class ProfilChangeWindow{
+  BuildContext context;
+  String titel;
+  Widget changeWidget;
+  Function saveFunction;
+
+  ProfilChangeWindow({required this.context, required this.titel,
+    required this.changeWidget, required this.saveFunction});
+
+
+  _topBar(){
+    return Row(
+      children: [
+        TextButton(
+          style: global_style.textButtonStyle(),
+          child: const Icon(Icons.arrow_back),
+          onPressed: () => Navigator.pop(context),
         ),
-        child: Icon(Icons.arrow_back),
-        onPressed: () => Navigator.pop(context),
-      ),
-      Expanded(child: Center(child: Text(titel))),
-      TextButton(
-          style: ButtonStyle(
-              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20.0),
-                  )
-              )
-          ),
-          child: Icon(Icons.done),
-          onPressed: (){
-            saveFunction();
-          }
-      ),
-    ],
-  );
-}
+        Expanded(child: Center(child: Text(titel))),
+        TextButton(
+            style: global_style.textButtonStyle(),
+            child: const Icon(Icons.done),
+            onPressed: (){
+              saveFunction();
+            }
+        ),
+      ],
+    );
+  }
 
-profilChangeWindow(context, String titel, Widget changeWidget, saveFunction){
-  return showDialog(
-      context: context,
-      builder: (BuildContext buildContext){
-        return AlertDialog(
-          contentPadding: EdgeInsets.fromLTRB(10, 20, 10, 20),
-          content: Scaffold(
-            body: Container(
-              height: 400,
-              width: double.maxFinite,
-              child: Column(
-                children: [
-                  _topBar(buildContext, titel, saveFunction),
-                  changeWidget
-                ],
+  profilChangeWindow(){
+    return showDialog(
+        context: context,
+        builder: (BuildContext buildContext){
+          return AlertDialog(
+            contentPadding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
+            content: Scaffold(
+              body: SizedBox(
+                height: 400,
+                width: double.maxFinite,
+                child: Column(
+                  children: [
+                    _topBar(),
+                    changeWidget
+                  ],
+                ),
               ),
             ),
-          ),
-        );
-      }
-  );
+          );
+        }
+    );
+  }
+
 }
 
 
--- lib/windows/profil_popup_window.dart
+++ lib/windows/profil_popup_window.dart
@@ -1,156 +1,191 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-import '../global/global_functions.dart' as globalFunctions;
-import '../global/global_functions.dart';
+import '../global/global_functions.dart' as global_functions;
+import '../global/style.dart' as global_style;
 import '../pages/chat/chat_details.dart';
 import '../services/database.dart';
 
 
-_menuBarProfil(context,userName,profilName, addFriendButton){
-  return Row(
-    children: [
-      TextButton(
-        style: ButtonStyle(
-            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20.0),
-                )
-            )
+class ProfilPopupWindow{
+  dynamic globalSetState;
+  dynamic context;
+  String userName;
+  var userID = FirebaseAuth.instance.currentUser!.uid;
+  Map profil;
+  Map userFriendlist;
+
+
+  ProfilPopupWindow({required this.context, required this.userName,
+    required this.profil,required this.userFriendlist});
+
+  _menuBarProfil(){
+    return Row(
+      children: [
+        TextButton(
+          style: global_style.textButtonStyle(),
+          child: const Icon(Icons.arrow_back),
+          onPressed: () => Navigator.pop(context),
         ),
-        child: Icon(Icons.arrow_back),
-        onPressed: () => Navigator.pop(context),
-      ),
-      Expanded(child: SizedBox()),
-      TextButton(
-          style: ButtonStyle(
-              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20.0),
-                  )
-              )
-          ),
-          child: Icon(Icons.message),
-          onPressed: () async {
-            var userID = FirebaseAuth.instance.currentUser!.uid;
-            var profilID = await ProfilDatabaseKontroller().getProfilIDFromName(profilName);
-            var users = [userID, profilID];
-            users.sort();
+        const Expanded(child: SizedBox()),
+        _messageButton(),
+        _friendButton()
+        //addFriendButton,
+      ],
+    );
+  }
 
-            var groupChatData = await ChatDatabaseKontroller().getChat(users.join("_"));
+  _messageButton(){
+    return TextButton(
+        style: global_style.textButtonStyle(),
+        child: const Icon(Icons.message),
+        onPressed: () async {
+          var profilID = await ProfilDatabaseKontroller().getProfilIDFromName(profil["name"]);
+          var users = [userID, profilID];
+          var newChat = false;
 
-            if(groupChatData != null){
-              changePage(context, ChatDetailsPage(
-                  groupChatData: groupChatData,
-                chatPartner: {profilID: profilName}
-              ));
-            } else {
-              changePage(context, ChatDetailsPage(
-                  groupChatData: {"users": {
-                    profilID: profilName,
-                    userID: userName
-                  }},
-                  chatPartner: {profilID: profilName},
-                  newChat: true
-              ));
-            }
+          var groupChatData = await ChatDatabaseKontroller()
+              .getChat(global_functions.getChatID(users));
 
+          if(groupChatData == null){
+            newChat = true;
+            groupChatData = {
+              "users": {
+                profilID: profil["name"],
+                userID: userName
+              }
+            };
           }
-      ),
-      addFriendButton,
-    ],
-  );
-}
 
-_titelProfil(profil){
-  return Center(
-      child:
-      Text(
-        profil["name"],
-        style: TextStyle(
-          fontSize: 24
-        ),
-      )
-  );
-}
+          global_functions.changePage(context, ChatDetailsPage(
+            groupChatData: groupChatData,
+            chatPartner: {profilID: profil["name"]},
+            newChat: newChat,
+          ));
+        }
+    );
+  }
 
-_infoProfil(profil){
-  var childrenList = profil["kinder"];
-  var childrenAgeList = [];
-  double columnAbstand = 5;
+  _friendButton(){
+    var onFriendlist = userFriendlist.keys.contains(profil["name"]);
+
+    return TextButton(
+        style: global_style.textButtonStyle(),
+        child: onFriendlist ? const Icon(Icons.person_remove) : const Icon(Icons.person_add),
+        onPressed: (){
+
+          if(onFriendlist){
+            userFriendlist.remove(profil["name"]);
+            if(userFriendlist.keys.isEmpty) userFriendlist = {"empty": true};
+          } else {
+            if(userFriendlist["empty"] == true) userFriendlist = {profil["name"]: true};
+          }
+
+          ProfilDatabaseKontroller().updateProfil(
+              userID, {"friendlist": userFriendlist}
+          );
+
+          globalSetState(() => userFriendlist.keys.contains(profil["name"]));
+
+        }
+    );
+  }
+
+  _titelProfil(){
+    return Center(
+        child:
+        Text(
+          profil["name"],
+          style: const TextStyle(
+              fontSize: 24
+          ),
+        )
+    );
+  }
+
+  _infoProfil(){
+    var childrenProfilList = profil["kinder"];
+    var childrenList = [];
+    double columnAbstand = 5;
+
+    childrenProfilList.forEach((child){
+      childrenList.add(global_functions.ChangeTimeStamp(child).intoYears()
+          .toString()+"J");
+    });
 
 
+    return Container(
+        padding: const EdgeInsets.only(left: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [ const
+            Text("Info", style: TextStyle(color: Colors.blue)),
+            SizedBox(height: columnAbstand),
+            Text("Ort: " + profil["ort"]),
+            SizedBox(height: columnAbstand),
+            Text("Reiseart: " + profil["reiseart"]),
+            SizedBox(height: columnAbstand),
+            Text("Sprache: " + profil["sprachen"].join(" , ")),
+            SizedBox(height: columnAbstand),
+            Text("Kinder: " + childrenList.join(" , ")),
+            SizedBox(height: columnAbstand),
+            Text("Interessen: " + profil["interessen"].join(" , ")),
+            SizedBox(height: columnAbstand),
+            Text("Über mich: " + profil["aboutme"]),
+          ],
+        )
+    );
+  }
 
-
-  childrenList.forEach((child){
-    childrenAgeList.add(globalFunctions.timeStampToAllDict(child)["years"]);
-  });
-
-
-  return Container(
-      padding: EdgeInsets.only(left: 10),
+  _kontaktProfil(){
+    return Container(
+      padding: const EdgeInsets.only(left: 10),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text("Info", style: TextStyle(color: Colors.blue)),
-          SizedBox(height: columnAbstand),
-          Text("Ort: " + profil["ort"]),
-          SizedBox(height: columnAbstand),
-          Text("Reiseart: " + profil["reiseart"]),
-          SizedBox(height: columnAbstand),
-          Text("Sprache: " + profil["sprachen"].join(" , ")),
-          SizedBox(height: columnAbstand),
-          Text("Kinder: " + childrenAgeList.join(" , ")),
-          SizedBox(height: columnAbstand),
-          Text("Interessen: " + profil["interessen"].join(" , ")),
-          SizedBox(height: columnAbstand),
-          Text("Über mich: " + profil["aboutme"]),
-        ],
-      )
-  );
-}
-
-_kontaktProfil(profil){
-  return Container(
-    padding: EdgeInsets.only(left: 10),
-    child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
+        children: [ const
           Text("Kontakt",style: TextStyle(color: Colors.blue),),
-          profil["emailAnzeigen"]?Text("Email: " + profil["email"]): Container()
+          profil["emailAnzeigen"] ?
+            Text("Email: " + profil["email"]) : const SizedBox.shrink()
         ],
       ),
-  );
+    );
+  }
+
+  profilPopupWindow(){
+
+    return showDialog(
+        context: context,
+        builder: (BuildContext buildContext){
+          return StatefulBuilder(
+              builder: (context, setState){
+                globalSetState = setState;
+                return AlertDialog(
+                  backgroundColor: Colors.white,
+                  contentPadding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
+                  content: SizedBox(
+                    height: 400,
+                    width: double.maxFinite,
+                    child: Scrollbar(
+                      thumbVisibility: true,
+                      child: ListView(
+                          children: [
+                            _menuBarProfil(),
+                            const SizedBox(height: 25),
+                            _titelProfil(),
+                            const SizedBox(height: 30),
+                            _infoProfil(),
+                            const SizedBox(height: 30),
+                            _kontaktProfil(),
+                          ]
+                      ),
+                    ),
+                  ),
+                );
+              });
+        }
+    );
+  }
+
 }
 
-profilPopupWindow(context,userName, profil, {addFriendButton}){
-
-  return showDialog(
-      context: context,
-      builder: (BuildContext context){
-        return AlertDialog(
-          backgroundColor: Colors.white,
-          contentPadding: EdgeInsets.fromLTRB(10, 20, 10, 20),
-          content: Container(
-            height: 400,
-            width: double.maxFinite,
-            child: Scrollbar(
-              isAlwaysShown: true,
-              child: ListView(
-                  children: [
-                    _menuBarProfil(context,userName,profil["name"], addFriendButton),
-                    SizedBox(height: 25),
-                    _titelProfil(profil),
-                    SizedBox(height: 30),
-                    _infoProfil(profil),
-                    SizedBox(height: 30),
-                    _kontaktProfil(profil),
-                  ]
-              ),
-            ),
-          ),
-        );
-      }
-  );
-}
 
