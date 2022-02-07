106153f055fe2b2a423dad1c47798a8fba9860c9
derdrache <dominik.mast.11@gmail.com>
2022-02-06 20:45:26 -0500

    add patchnotes/updates Window

--- lib/windows/patchnotes.dart
+++ lib/windows/patchnotes.dart
@@ -1,32 +1,57 @@
 import 'package:familien_suche/global/custom_widgets.dart';
 import 'package:flutter/material.dart';
 
+
 class PatchnotesWindow{
   var context;
+  var patchnotesTitle = "Patchnotes";
 
-  PatchnotesWindow({this.context});
+  PatchnotesWindow({required this.context});
 
 
-  openWindow(){
-    return showDialog(
-        context: context,
-        builder: (BuildContext buildContext){
-          return AlertDialog(
-            contentPadding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
-            content: Scaffold(
-              body: SizedBox(
-                height: 400,
-                width: double.maxFinite,
-                child: ListView(
-                  children: [
-                    WindowTopbar(title: "Patchnotes"),
-                    Text("1.0.0")
-                  ],
-                ),
+  _patch(title, inhalt){
+    return Container(
+      margin: EdgeInsets.only(top: 15, left: 10, right: 5),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Text(
+              title,
+              style: TextStyle(
+                fontSize: 15,
+                  fontWeight: FontWeight.bold
               ),
             ),
-          );
-        }
+            SizedBox(width: 10),
+            SizedBox(
+              width: 200,
+              child: Text(
+                inhalt,
+                maxLines: null,
+                style: TextStyle(fontSize: 15),
+              ),
+            )
+          ]
+        )
     );
   }
+
+  openWindow(){
+    return CustomWindow(
+        context: context,
+        title: patchnotesTitle,
+        children: [
+          _patch(patch1["title"], patch1["inhalt"])
+        ]
+    );
+  }
+
 }
+
+
+var patch1 = {
+  "title" : "1.0.0",
+  "inhalt": "App veröffentlicht"
+};
+
--- lib/windows/upcoming_updates.dart
+++ lib/windows/upcoming_updates.dart
@@ -1,30 +1,46 @@
 import 'package:flutter/material.dart';
 
-class UpcomingUpdatesWindow{
+import '../global/custom_widgets.dart';
+
+class UmcomingUpdatesWindow{
   var context;
+  var patchnotesTitle = "Geplante Updates";
 
-  UpcomingUpdatesWindow({this.context});
+  UmcomingUpdatesWindow({this.context});
 
 
-  openWindow(){
-    return showDialog(
-        context: context,
-        builder: (BuildContext buildContext){
-          return AlertDialog(
-            contentPadding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
-            content: Scaffold(
-              body: SizedBox(
-                height: 400,
-                width: double.maxFinite,
-                child: ListView(
-                  children: [
-                    Text("Test")
-                  ],
-                ),
-              ),
-            ),
-          );
-        }
+  _update(title){
+    return Container(
+      width: 200,
+      margin: EdgeInsets.only(left: 10, top: 10),
+      child: Text(
+        "- " + title,
+        style: TextStyle(fontSize: 15),
+        maxLines: null,
+      )
     );
   }
+
+  openWindow(){
+    return CustomWindow(
+        context: context,
+        title: patchnotesTitle,
+        children: [
+          _update("Englische Version"),
+          _update("IOS Version"),
+          _update("Account löschen"),
+          _update("Automatischer Standort"),
+          _update("Anonyme Anmelden"),
+          _update("Events eintragen"),
+          _update("Gemeinschaften Eintragen"),
+          _update("Layout verbessern"),
+          _update("Freunde auf Karte farblich markieren"),
+          _update("Mehrere Accounts verbinden"),
+          _update("Chat erweiterungen"),
+          _update("Chatgruppen"),
+          _update("Eventboard"),
+        ]
+    );
+  }
+
 }
