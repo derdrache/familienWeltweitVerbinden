a629964a85b3dd91c01866dc34a1253d33a2d520
derdrache <dominik.mast.11@gmail.com>
2022-02-24 09:29:16 -0500

    patch 1.1.1

--- lib/windows/upcoming_updates.dart
+++ lib/windows/upcoming_updates.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import '../global/custom_widgets.dart';
 
 class UmcomingUpdatesWindow{
   var context;
-  var patchnotesTitle = "Geplante Updates";
 
   UmcomingUpdatesWindow({this.context});
 
 
   _update(title){
+
     return Container(
       width: 200,
       margin: EdgeInsets.only(left: 10, top: 10),
@@ -22,24 +22,26 @@ class UmcomingUpdatesWindow{
   }
 
   openWindow(){
+    var patchnotesTitle = AppLocalizations.of(context).geplanteErweiterungen;
+
     return CustomWindow(
         context: context,
         title: patchnotesTitle,
         children: [
-          _update("IOS Version / Web Version"),
-          _update("Mehrere Accounts verbinden"),
-          _update("Automatischer Standort"),
-          _update("Nutzer blockieren"),
-          _update("Events planen"),
-          _update("Reiseplanung"),
-          _update("Gemeinschaften Eintragen"),
-          _update("Freunde auf Karte farblich markieren"),
-          _update("Chatgruppen"),
-          _update("Eventboard"),
-          _update("Anonyme Anmelden"),
-          _update("Chat erweiterungen"),
-          _update("Account löschen"),
-          _update("Layout verbessern"),
+          _update("Web Version"),
+          _update(AppLocalizations.of(context).familienAnzeige),
+          _update(AppLocalizations.of(context).automatischerStandort),
+          _update(AppLocalizations.of(context).nutzerBlockieren),
+          _update(AppLocalizations.of(context).eventsPlanen),
+          _update(AppLocalizations.of(context).reisePlanung),
+          _update(AppLocalizations.of(context).gemeinschaftenUpdate),
+          _update(AppLocalizations.of(context).freundeMarkieren),
+          _update(AppLocalizations.of(context).chatgruppen),
+          _update(AppLocalizations.of(context).eventboard),
+          _update(AppLocalizations.of(context).anonymeAnmelden),
+          _update(AppLocalizations.of(context).chatErweiterung),
+          _update(AppLocalizations.of(context).accountLoeschen),
+          _update(AppLocalizations.of(context).layoutVerbessern),
         ]
     );
   }
