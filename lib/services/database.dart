afe17478c63ebd6a1bb6886be2d225d8385274e5
derdrache <dominik.mast.11@gmail.com>
2022-04-19 10:27:38 -0500

    Patch 1.2.2

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,7 +1,3 @@
-import 'dart:math';
-
-import 'package:crypto/crypto.dart';
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart' hide Key;
 import 'package:http/http.dart' as http;
