130a6dbb0a2c87e54fc5f0268c92a388220d55c6
derdrache <dominik.mast.11@gmail.com>
2022-05-07 12:37:06 -0500

    automatic location

--- lib/pages/settings/privacy_security_page.dart
+++ lib/pages/settings/privacy_security_page.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+import 'dart:ui';
+import 'package:familien_suche/global/variablen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
@@ -17,28 +21,60 @@ class PrivacySecurityPage extends StatefulWidget {
 }
 
 class _PrivacySecurityPageState extends State<PrivacySecurityPage> {
-  var userID = FirebaseAuth.instance.currentUser.uid;
+  var userId = FirebaseAuth.instance.currentUser.uid;
+  double fontsize = 20;
+  var automaticLocationDropdown = CustomDropDownButton();
+  var spracheIstDeutsch = kIsWeb ? window.locale.languageCode == "de" : Platform.localeName == "de_DE";
+
+  saveAutomaticLocation(){
+    ProfilDatabase().updateProfil(
+        "automaticLocation ='${automaticLocationDropdown.getSelected()}'",
+        "WHERE id ='$userId'"
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
+    var locationList = spracheIstDeutsch ? standortbestimmung : standortbestimmungEnglisch;
+    automaticLocationDropdown = CustomDropDownButton(
+      items: locationList,
+      selected: widget.profil["automaticLocation"] ?? (spracheIstDeutsch ?
+        standortbestimmung[0] : standortbestimmungEnglisch[0]),
+      onChange: () => saveAutomaticLocation(),
+    );
 
     emailSettingContainer(){
-      return Row(
-        children: [
-          const SizedBox(width: 20),
-          Text(AppLocalizations.of(context).emailAlleSichtbar, style: const TextStyle(fontSize: 20),),
-          const Expanded(child: SizedBox(width: 20)),
-          Switch(
-              value: widget.profil["emailAnzeigen"] == 1 ? true: false,
-              onChanged: (value){
-                setState(() {
-                  widget.profil["emailAnzeigen"] = value == true ? 1 : 0;
-                });
-                ProfilDatabase().updateProfil(
-                  "emailAnzeigen = '${widget.profil["emailAnzeigen"]}'",
-                  "WHERE id = '$userID'");
-              })
-        ],
+      return Container(
+        margin: EdgeInsets.all(10),
+        child: Row(
+          children: [
+            Text(AppLocalizations.of(context).emailAlleSichtbar, style: TextStyle(fontSize: fontsize),),
+            const Expanded(child: SizedBox(width: 20)),
+            Switch(
+                value: widget.profil["emailAnzeigen"] == 1 ? true: false,
+                onChanged: (value){
+                  setState(() {
+                    widget.profil["emailAnzeigen"] = value == true ? 1 : 0;
+                  });
+                  ProfilDatabase().updateProfil(
+                    "emailAnzeigen = '${widget.profil["emailAnzeigen"]}'",
+                    "WHERE id = '$userId'");
+                })
+          ],
+        ),
+      );
+    }
+
+    automaticLocationContainer(){
+      return Container(
+        margin: EdgeInsets.all(10),
+        child: Row(
+          children: [
+            SizedBox(width: 200,child: Text("Automatische Standortbestimmung", style: TextStyle(fontSize: fontsize),)),
+            Expanded(child: SizedBox()),
+            SizedBox(width: 150, child: automaticLocationDropdown)
+          ],
+        ),
       );
     }
 
@@ -59,8 +95,8 @@ class _PrivacySecurityPageState extends State<PrivacySecurityPage> {
       appBar: customAppBar(title: AppLocalizations.of(context).privatsphaereSicherheit),
       body: Column(
         children: [
-          const SizedBox(height: 20,),
           emailSettingContainer(),
+          automaticLocationContainer(),
           const Expanded(child: SizedBox.shrink()),
           //deleteProfilContainer(),
           //SizedBox(height: 10)
