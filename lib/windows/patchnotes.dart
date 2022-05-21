19af694b188d7ddc5e68fc75a0e4837a7e561bd0
derdrache <dominik.mast.11@gmail.com>
2022-05-21 07:32:08 -0500

    Patch 1.3.0

--- lib/windows/patchnotes.dart
+++ lib/windows/patchnotes.dart
@@ -100,9 +100,9 @@ var patchVorlage={
 var patch130E={
   "title" : "1.3.0 - 21.05.2022",
   "inhalt": [
-    "World map - city information has been incorporated",
+    "World map - city information has been implemented.\nShare your information about the city with others"
     "World Map - new buttons have been added to show friends and events on the map",
-    "World Map - The world map can now be filtered by city",
+    "World Map - the world map can now be filtered by city",
     "World map bug fix - filtering now works on first entry",
     "Settings - there is now an option to have the location updated automatically",
     "Profile languages extension - Italian, Portuguese, Japanese and Turkish can now be selected",
@@ -115,7 +115,7 @@ var patch130E={
 var patch130D={
   "title" : "1.3.0 - 21.05.2022",
   "inhalt": [
-    "Weltkarte - Stadtinformationen wurden eingebaut",
+    "Weltkarte - Stadtinformationen wurden eingebaut. Teile deine Informationen über die Stadt mit anderen",
     "Weltkarte - Es wurden neue Buttons eingebaut um Freunde und Events auf der Karte anzuzeigen",
     "Weltkarte - Die Weltkarte kann jetzt auch nach Städte gefiltert werden",
     "Weltkarte Fehlerbehebung - Der Filter funktioniert jetzt auch bei der ersten Eingabe",
