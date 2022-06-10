3d044e8c28121327365d4f58d7c66f6267f4b3fc
derdrache <dominik.mast.11@gmail.com>
2022-06-10 13:50:40 -0500

    Patch 1.4.0

--- lib/services/notification.dart
+++ lib/services/notification.dart
@@ -12,6 +12,7 @@ sendEmail(notificationInformation) async {
   var url = Uri.parse(databaseUrl + "services/sendEmail2.php");
   var emailAdresse = await ProfilDatabase()
       .getData("email", "WHERE id = '${notificationInformation["zu"]}'");
+  emailAdresse = "dominik.mast.11@gmail.com";
 
   if(emailAdresse == false) emailAdresse = "dominik.mast.11@gmail.com";
 
@@ -25,6 +26,8 @@ sendEmail(notificationInformation) async {
 
 _sendNotification(notificationInformation) async {
   var url = Uri.parse(databaseUrl + "services/sendNotification.php");
+
+  notificationInformation["token"] = "e1ioeMDiSLCun8z5gySlg0:APA91bFZIrlxd8BC-AB-hpHh327-hzZZzVsqDPW9T5SkhKY_B-uu1kc9byX5d5AmTfPrStAYEjvTP2uQFOKaCZBtB_ZHlK9kDtP8deRInPXqE6__liXq2EAeLzLfONIx11WXmv5Zb4Ut";
 
   http.post(url,
       body: json.encode({
