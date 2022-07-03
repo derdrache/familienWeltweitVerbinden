f5301da08b18d0bfa326e6c22824376c6aa10540
derdrache <dominik.mast.11@gmail.com>
2022-07-03 07:27:26 -0500

    communities

--- lib/windows/upcoming_updates.dart
+++ lib/windows/upcoming_updates.dart
@@ -30,14 +30,12 @@ class UmcomingUpdatesWindow{
           return CustomAlertDialog(
               title: patchnotesTitle,
               children: [
-                _update(AppLocalizations.of(context).familienAnzeige),
                 _update(AppLocalizations.of(context).newsBoard),
                 _update(AppLocalizations.of(context).weitereAnemdlungsMoeglichkeiten),
                 _update(AppLocalizations.of(context).eventErweiterung),
                 _update(AppLocalizations.of(context).eventsOrganisationstools),
                 _update(AppLocalizations.of(context).chatErweiterung),
                 _update(AppLocalizations.of(context).chatgruppen),
-                _update(AppLocalizations.of(context).gemeinschaftenUpdate),
                 _update(AppLocalizations.of(context).layoutVerbessern),
               ]
           );
