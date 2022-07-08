1d4baaa06dc0efa70e72850947f89f5a31d9c4fb
derdrache <dominik.mast.11@gmail.com>
2022-07-08 06:26:00 -0500

    community erstellen

---
+++ lib/pages/community/community_erstellen.dart
@@ -0,0 +1,89 @@
+import 'package:familien_suche/global/custom_widgets.dart';
+import 'package:familien_suche/global/global_functions.dart' as global_func;
+import 'package:familien_suche/services/database.dart';
+import 'package:familien_suche/widgets/custom_appbar.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+import '../../widgets/google_autocomplete.dart';
+import '../start_page.dart';
+
+class CommunityErstellen extends StatefulWidget {
+  const CommunityErstellen({Key key}) : super(key: key);
+
+  @override
+  State<CommunityErstellen> createState() => _CommunityErstellenState();
+}
+
+class _CommunityErstellenState extends State<CommunityErstellen> {
+  var nameController = TextEditingController();
+  var beschreibungKontroller = TextEditingController();
+  var linkKontroller = TextEditingController();
+  var ortAuswahlBox = GoogleAutoComplete();
+
+  
+
+  saveCommunity(){
+    var locationData = ortAuswahlBox.getGoogleLocationData();
+    var communityData = {
+      "name" : nameController.text,
+      "beschreibung": beschreibungKontroller.text,
+      "link": linkKontroller.text,
+      "ort": locationData["city"],
+      "land": locationData["countryname"],
+      "latt": locationData["latt"],
+      "longt": locationData["longt"],
+      "erstelltAm": DateTime.now().toString(),
+      "erstelltVon": FirebaseAuth.instance.currentUser.displayName
+    };
+
+    if(!checkValidationAndSendError(communityData)) return false;
+
+    CommunityDatabase().addNewCommunity(communityData);
+
+    return true;
+
+  }
+
+  checkValidationAndSendError(communityData){
+    if(communityData["name"].isEmpty){
+      customSnackbar(context, AppLocalizations.of(context).bitteNameEingeben);
+    }else if(communityData["beschreibung"].isEmpty){
+      customSnackbar(context, AppLocalizations.of(context).bitteCommunityBeschreibungEingeben);
+    }else if(communityData["ort"].isEmpty){
+      customSnackbar(context, AppLocalizations.of(context).bitteStadtEingeben);
+    } else{
+      return true;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    ortAuswahlBox.hintText = AppLocalizations.of(context).stadtEingeben;
+
+    return Scaffold(
+      appBar: CustomAppBar(
+        title: AppLocalizations.of(context).communityErstellen,
+          buttons: [
+            IconButton(
+                onPressed: () {
+                  var success = saveCommunity();
+                  if(!success) return;
+
+                  global_func.changePage(context, StartPage(selectedIndex: 2));
+                  //global_func.changePage(context, );
+    },
+                icon: const Icon(Icons.done, size: 30))
+          ]),
+      body: ListView(
+        children: [
+          customTextInput(AppLocalizations.of(context).communityName, nameController),
+          ortAuswahlBox,
+          customTextInput(AppLocalizations.of(context).linkEingebenOptional, linkKontroller),
+          customTextInput(AppLocalizations.of(context).beschreibungCommunity, beschreibungKontroller, moreLines: 5, textInputAction: TextInputAction.newline)
+        ],
+      ),
+    );
+  }
+}
