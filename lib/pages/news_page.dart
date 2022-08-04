1f6b22e06e245522a983f85045ff11021b696b4a
derdrache <dominik.mast.11@gmail.com>
2022-08-04 06:40:35 -0500

    patch 1.6.0

--- lib/pages/news_page.dart
+++ lib/pages/news_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:hive/hive.dart';
 
 import '../global/variablen.dart' as global_var;
 import '../services/database.dart';
@@ -13,18 +14,39 @@ class NewsPage extends StatefulWidget{
 }
 
 class _NewsPageState extends State<NewsPage>{
+  var newsFeedData = Hive.box('secureBox').get("newsFeed");
+  List<Widget> newsFeed = [];
 
+  @override
+  void initState() {
+    refreshNewsFeed();
+    createNewsFeed();
+
+    super.initState();
+  }
+
+  refreshNewsFeed() async {
+    List<dynamic> dbNewsData =
+        await NewsFeedDatabase().getData("*", "ORDER BY date ASC");
+    if (dbNewsData == false) dbNewsData = [];
+
+    Hive.box('secureBox').put("newsFeed", dbNewsData);
+  }
+
+  createNewsFeed(){
+
+  }
 
   Widget build(BuildContext context){
 
     iconBox(icon){
       return Container(
-        margin: EdgeInsets.all(15),
+        margin: const EdgeInsets.all(15),
         height: 25,
         width: 50,
         decoration: BoxDecoration(
           color: Theme.of(context).colorScheme.primary,
-          borderRadius: BorderRadius.all(Radius.circular(10))
+          borderRadius: const BorderRadius.all(Radius.circular(10))
 
         ),
         child: Icon(icon, color: Colors.white,size: 24,)
@@ -52,12 +74,24 @@ class _NewsPageState extends State<NewsPage>{
       );
     }
 
+    headBox(){
+      return Container(
+        padding: EdgeInsets.all(10),
+        child: Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+          IconButton(
+            icon: Icon(Icons.settings),
+            onPressed: null,
+          )
+        ],),
+      );
+    }
+
     newsFeedBox(){
       return Container(
+
         child: ListView(
-          children: [
-            Center(child: Text("test")),
-          ],
+          shrinkWrap: true,
+          children: newsFeed,
         )
       );
     }
@@ -67,10 +101,8 @@ class _NewsPageState extends State<NewsPage>{
         padding: const EdgeInsets.only(top: kIsWeb? 0: 24),
         child: Column(
           children: [
-            friendsSliderBox(),
-            Expanded(
-                child: newsFeedBox()
-            )
+            headBox(),
+            newsFeedBox()
           ],
         ),
       )
