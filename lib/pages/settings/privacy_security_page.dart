4586fe4866f58ce45833a8c0168911d00de3ede6
derdrache <dominik.mast.11@gmail.com>
2022-08-27 07:06:47 -0500

    patch 1.6.0

--- lib/pages/settings/privacy_security_page.dart
+++ lib/pages/settings/privacy_security_page.dart
@@ -232,7 +232,7 @@ class _PrivacySecurityPageState extends State<PrivacySecurityPage> {
                   child: const Text("Ok"),
                   onPressed: () {
                     ProfilDatabase().deleteProfil(widget.profil["id"]);
-                    dbDeleteImage(widget.profil["bild"]);
+                    DbDeleteImage(widget.profil["bild"]);
                     setState(() {});
                     global_functions.changePageForever(
                         context, const LoginPage());
