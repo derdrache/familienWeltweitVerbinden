5034be07699004dc239afdeae4389a4de877cf5f
derdrache <dominik.mast.11@gmail.com>
2022-12-04 07:09:03 -0500

    1.9.0

--- lib/pages/informationen/information.dart
+++ lib/pages/informationen/information.dart
@@ -1,41 +1,152 @@
 import 'package:flutter/material.dart';
 import 'package:familien_suche/global/global_functions.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import '../informationen/community/community_page.dart';
 import '../informationen/events/event_page.dart';
 import '../informationen/stadtinformation/city_page.dart';
 
-class InformationPage extends StatelessWidget {
+
+class InformationPage extends StatefulWidget {
   const InformationPage({Key key}) : super(key: key);
 
   @override
+  State<InformationPage> createState() => _InformationPageState();
+}
+
+class _InformationPageState extends State<InformationPage> {
+  @override
   Widget build(BuildContext context) {
 
-    pageCards(title, icon, page){
+    pageCards(title, icon, image, page) {
       return GestureDetector(
         onTap: () => changePage(context, page),
         child: Container(
-          margin: const EdgeInsets.only(left: 10, right: 10, top: 30, bottom: 30),
+          margin:
+          const EdgeInsets.only(left: 10, right: 10, top: 30, bottom: 30),
           child: Card(
             elevation: 25,
             shadowColor: Colors.black,
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(15.0),
             ),
-            color: Theme.of(context).colorScheme.primary,
             child: Container(
-              width: 150,
-              height: 220,
-              padding: const EdgeInsets.all(10),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(icon, color: Colors.white,size: 50,),
-                  const SizedBox(height: 30),
-                  Text(title, style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-                ],
-              )
+                width: 150,
+                height: 220,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15.0),
+                    image: image == null ? null : DecorationImage(
+                        fit: BoxFit.fitHeight,
+                        colorFilter: ColorFilter.mode(
+                            Colors.black.withOpacity(0.8), BlendMode.dstATop),
+                        image: AssetImage(image))),
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      icon,
+                      color: Colors.black,
+                      size: 50,
+                    ),
+                    const SizedBox(height: 30),
+                    Text(
+                      title,
+                      style: const TextStyle(
+                          fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                )),
+          ),
+        ),
+      );
+    }
+
+    return EventPage();
+
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                pageCards(
+                    "Events",
+                    Icons.calendar_month,
+                    "assets/bilder/museum.jpg",
+                    const EventPage()),
+                pageCards(
+                    "Communities",
+                    Icons.home,
+                    "assets/bilder/village.jpg",
+                    const CommunityPage())
+              ],
             ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                pageCards(
+                    AppLocalizations.of(context).cities,
+                    Icons.location_city,
+                    "assets/bilder/city.jpg",
+                    const CityPage()),
+                //pageCards("Länder", Icons.flag, null)
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+/*
+class InformationPage extends StatelessWidget {
+  const InformationPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    pageCards(title, icon, image, page) {
+      return GestureDetector(
+        onTap: () => changePage(context, page),
+        child: Container(
+          margin:
+              const EdgeInsets.only(left: 10, right: 10, top: 30, bottom: 30),
+          child: Card(
+            elevation: 25,
+            shadowColor: Colors.black,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15.0),
+            ),
+            child: Container(
+                width: 150,
+                height: 220,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15.0),
+                    image: image == null ? null : DecorationImage(
+                        fit: BoxFit.fitHeight,
+                        colorFilter: ColorFilter.mode(
+                            Colors.black.withOpacity(0.8), BlendMode.dstATop),
+                        image: AssetImage(image))),
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      icon,
+                      color: Colors.black,
+                      size: 50,
+                    ),
+                    const SizedBox(height: 30),
+                    Text(
+                      title,
+                      style: const TextStyle(
+                          fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                )),
           ),
         ),
       );
@@ -46,17 +157,37 @@ class InformationPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-              pageCards("Events", Icons.calendar_month, const EventPage()),
-              pageCards("Communities", Icons.home, const CommunityPage())
-            ],),
-            Row(mainAxisAlignment: MainAxisAlignment.center,children: [
-              pageCards("Städte", Icons.location_city, null),
-              //pageCards("Länder", Icons.flag, null)
-            ],)
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                pageCards(
+                    "Events",
+                    Icons.calendar_month,
+                    "assets/bilder/museum.jpg",
+                    const EventPage()),
+                pageCards(
+                    "Communities",
+                    Icons.home,
+                    "assets/bilder/village.jpg",
+                    const CommunityPage())
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                pageCards(
+                    AppLocalizations.of(context).cities,
+                    Icons.location_city,
+                    "assets/bilder/city.jpg",
+                    const CityPage()),
+                //pageCards("Länder", Icons.flag, null)
+              ],
+            )
           ],
         ),
       ),
     );
   }
 }
+
+ */
