3b1584bf34adfd1328ac8a4a26bd785f95267165
derdrache <dominik.mast.11@gmail.com>
2022-12-19 23:45:31 -0600

    iOS Update

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -2,26 +2,24 @@ import UIKit
 import Flutter
 import Firebase
 import FirebaseMessaging
-import flutter_local_notifications
 
 @UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate, MessagingDelegate {
+@objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    didFinishLaunchingWithOptions launchOptions:
+ [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     FirebaseApp.configure()
     GeneratedPluginRegistrant.register(with: self)
-    Messaging.messaging().delegate = self
-
-            UNUserNotificationCenter.current().delegate = self
-            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-            UNUserNotificationCenter.current().requestAuthorization(
-                    options: authOptions,
-                    completionHandler: {_, _ in })
-
-        application.registerForRemoteNotifications()
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    return super.application(application, didFinishLaunchingWithOptions:
+  launchOptions)
   }
+  override func application(_ application: UIApplication,
+  didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
 
+   Messaging.messaging().apnsToken = deviceToken
+   super.application(application,
+   didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
+ }
 }
---
+++ ios/Runner/AppDelegateOld.swift
@@ -0,0 +1,27 @@
+import UIKit
+import Flutter
+import Firebase
+import FirebaseMessaging
+import flutter_local_notifications
+
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate, MessagingDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    FirebaseApp.configure()
+    GeneratedPluginRegistrant.register(with: self)
+    Messaging.messaging().delegate = self
+
+    UNUserNotificationCenter.current().delegate = self
+    let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+    UNUserNotificationCenter.current().requestAuthorization(
+        options: authOptions,
+        completionHandler: {_, _ in })
+
+    application.registerForRemoteNotifications()
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+}
