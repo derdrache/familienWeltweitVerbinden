165093d0c3f3ba90aba160b0a525ea196c996330
derdrache <dominik.mast.11@gmail.com>
2022-12-26 02:09:24 -0500

    1.9.1

--- lib/pages/start_page.dart
+++ lib/pages/start_page.dart
@@ -72,6 +72,7 @@ class _StartPageState extends State<StartPage> with WidgetsBindingObserver {
   @override
   void dispose(){
     _networkConnectivity.disposeStream();
+    super.dispose();
   }
 
   @override
