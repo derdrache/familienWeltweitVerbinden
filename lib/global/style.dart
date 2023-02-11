347659cd59eb3bfa9d33ac217b5332124a08e71d
derdrache <dominik.mast.11@gmail.com>
2023-02-11 06:02:15 -0500

    web ui bugfix

--- lib/global/style.dart
+++ lib/global/style.dart
@@ -2,9 +2,21 @@ import 'package:flutter/material.dart';
 
 getResponsiveFontSize(context, fontType){
   double unitHeightValue = MediaQuery.of(context).size.width * 0.01;
+  double size;
 
-  if(fontType == "h1") return 5 * unitHeightValue;
-  if(fontType == "p") return 3.75 * unitHeightValue;
+  if(fontType == "h1"){
+    size = 5 * unitHeightValue;
+
+    if(size > 30) size = 30;
+
+    return size;
+  } else if(fontType == "p"){
+    size = 3.75 * unitHeightValue;
+
+    if(size > 22.5) size = 22.5;
+
+    return 3.75 * unitHeightValue;
+  }
 }
 
 textButtonStyle(){
