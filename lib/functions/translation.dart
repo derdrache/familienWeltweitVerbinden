92b4ef4c30b3efb33d87fa2ef1d8072bd8258760
dominik <dominik.mast.11@gmail.com>
2023-06-23 20:30:30 +0200

    add selectable translation text

--- lib/functions/translation.dart
+++ lib/functions/translation.dart
@@ -1,6 +1,6 @@
 import 'package:translator/translator.dart';
 
-translation(text) async{
+translation(text, {withTranslationNotice = false}) async{
   final translator = GoogleTranslator();
   var languageCheck = await translator.translate(text);
   bool isGerman = languageCheck.sourceLanguage.code == "de";
@@ -11,14 +11,16 @@ translation(text) async{
     var textTranslation = await translator.translate(text,
         from: "de", to: "auto");
 
-    textEng = textTranslation.toString() + "\n\nThis is an automatic translation";
+    textEng = textTranslation.toString();
+    if(withTranslationNotice) textEng += "\n\nThis is an automatic translation";
     textGer = text;
   } else {
     var textTranslation = await translator.translate(text,
         from: "auto", to: "de");
 
     textEng = text;
-    textGer = textTranslation.toString() + "\n\nDies ist eine automatische Übersetzung";
+    textGer = textTranslation.toString();
+    if(withTranslationNotice) textGer += "\n\nDies ist eine automatische Übersetzung";
   }
 
 
