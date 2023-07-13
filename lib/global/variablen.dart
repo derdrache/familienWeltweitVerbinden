81c80531d9e7c12608e3727cdc148f230fbbff2d
dominik <dominik.mast.11@gmail.com>
2023-07-13 05:58:48 +0200

    add intereset digital nomads

--- lib/global/variablen.dart
+++ lib/global/variablen.dart
@@ -7,7 +7,7 @@ List<String> reisearten = ["Fester Standort", "Flugzeug/Unterkünfte",
   "Auto/Unterkünfte", "Wohnmobile/Camping", "Boot"];
 List<String> interessenListe = ["Homeschooling", "Freilerner", "Worldschooling",
   "Montessori", "Waldorf", "Gemeinsame Aktivitäten", "Weltreise", "Langsam reisen", "Gemeinsam reisen",
-  "Gemeinsam wohnen", "Gemeinsame Kinderbetreuung", "Dauerhaft reisen", "Kaffeetreff", "Online kennenlernen",
+  "Gemeinsam wohnen", "Digitale Nomaden", "Gemeinsame Kinderbetreuung", "Dauerhaft reisen", "Kaffeetreff", "Online kennenlernen",
   "Minimalismus","Frugalismus", "Vegan", "Zuckerfrei","Glutenfrei", "Gewaltfreie Kommunikation"];
 List<String> sprachenListe = ["Deutsch", "Englisch", "Spanisch", "Chinesisch",
   "Französisch", "Arabisch", "Russisch", "Italienisch", "Portugiesisch", "Japanisch", "Türkisch", "Polnisch", "Niederländisch"];
@@ -21,7 +21,7 @@ List<String> privacySetting = ["Alle","Follower", "Freunde"];
 List<String> reiseartenEnglisch = ["fixed location", "airplane/housing",
   "car/housing","mobile home/camping", "boat"];
 List<String> interessenListeEnglisch = ["homeschooling", "unschooling", "worldschooling","montessori", "waldorf",
-  "joint activities", "world Travel", "travel slowly", "travel together", "co-living", "shared child care",
+  "joint activities", "world Travel", "travel slowly", "travel together", "co-living", "digital nomads", "shared child care",
   "open end travel", "coffee meeting", "meet online","minimalism","frugalism", "vegan","sugar free","gluten free",
   "nonviolent communication"];
 List<String> sprachenListeEnglisch = ["german", "english", "spanish", "chinese",
