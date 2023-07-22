6d3a8df4c3241e7ba228e2c69055bf49748aceb3
dominik <dominik.mast.11@gmail.com>
2023-07-22 06:25:49 +0200

    create colorful background

---
+++ lib/widgets/colorful_background.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+import '../global/style.dart' as Style;
+
+class ColorfulBackground extends StatelessWidget {
+  ColorfulBackground({
+    Key? key,
+    this.width = double.infinity,
+    this.height = double.infinity,
+    this.child,
+    this.colors = const [Colors.orange, Colors.orangeAccent]
+  });
+
+  Widget? child;
+  double width;
+  double height;
+  List colors;
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    List<Widget> decoratorsList(){
+      return [
+        Positioned(
+          bottom: 0,
+          right: 0,
+          child: Transform.rotate(
+            angle: math.pi / 4,
+            child: Container(
+              height: 200,
+              width: 300,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(Style.roundedCorners),
+                color: Colors.white.withOpacity(0.2),
+              ),
+            ),
+          ),
+        ) ,
+        Positioned(
+          top: 0,
+          right: -70,
+          child: Transform.rotate(
+            angle: math.pi / 4,
+            child: Container(
+              height: 100,
+              width: 150,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(Style.roundedCorners),
+                color: Colors.white.withOpacity(0.2),
+              ),
+            ),
+          ),
+        ) ,
+        Positioned(
+          top: 0,
+          left: -100,
+          child: Transform.rotate(
+            angle: -math.pi / 4,
+            child: Container(
+              height: 200,
+              width: 270,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(Style.roundedCorners),
+                color: Colors.white.withOpacity(0.1),
+              ),
+            ),
+          ),
+        ) ,
+      ];
+    }
+    
+    return SizedBox(
+      height: height,
+      width: width,
+      child: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [Colors.orange, Colors.orangeAccent]
+              )
+            ),
+          ),
+          for(var decoration in decoratorsList()) decoration,
+          Positioned.fill(child: child ?? const SizedBox())
+        ],
+      ),
+    );
+  }
+}
+
