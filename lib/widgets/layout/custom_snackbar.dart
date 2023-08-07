700f4019ab4f82dbce0aca46cfe505311e0c858e
dominik <dominik.mast.11@gmail.com>
2023-08-07 05:51:15 +0200

    change snackbar

--- lib/widgets/layout/custom_snackbar.dart
+++ lib/widgets/layout/custom_snackbar.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import '../../global/style.dart' as style;
 
 customSnackbar(context, text, {color = Colors.red, duration = const Duration(seconds: 3)}){
   var snackbar = SnackBar(
       duration: duration,
-      backgroundColor: color,
-      content: Text(text)
+      behavior: SnackBarBehavior.floating,
+      elevation: 0,
+      backgroundColor: Colors.transparent,
+      content: Container(
+        padding: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(style.roundedCorners),
+            color: color
+          ),
+          child: Text(text,style: TextStyle(fontWeight: FontWeight.bold),)
+      )
   );
 
   ScaffoldMessenger.of(context).hideCurrentSnackBar();
