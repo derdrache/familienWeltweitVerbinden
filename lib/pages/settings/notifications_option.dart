d3d542c5c2bfce4231c7e70c8c6f668b9f3318ee
dominik <dominik.mast.11@gmail.com>
2023-08-23 15:16:49 +0200

    add explanation text

--- lib/pages/settings/notifications_option.dart
+++ lib/pages/settings/notifications_option.dart
@@ -19,107 +19,133 @@ class _NotificationsOptionsPageState extends State<NotificationsOptionsPage> {
 
 
   allNotificationSetting() {
-    return Row(
-      children: [
-        const SizedBox(width: 20),
-        Text(
-            AppLocalizations.of(context)!.alleBenachrichtigungen,
-            style: const TextStyle(fontSize: 20)),
-        const Expanded(child: SizedBox(width: 20)),
-        Switch(
-            value: ownProfil["notificationstatus"] == 1 ? true : false,
-            onChanged: (value) {
-              var notificationOn = value == true ? 1 : 0;
+    return Container(
+      margin: const EdgeInsets.all(10),
+      child: Row(
+        children: [
+          Text(
+              AppLocalizations.of(context)!.alleBenachrichtigungen,
+              style: const TextStyle(fontSize: 20)),
+          const Expanded(child: SizedBox(width: 20)),
+          Switch(
+              value: ownProfil["notificationstatus"] == 1 ? true : false,
+              onChanged: (value) {
+                var notificationOn = value == true ? 1 : 0;
 
-              setState(() {
-                ownProfil["notificationstatus"] = notificationOn;
-                ownProfil["chatNotificationOn"] = notificationOn;
-                ownProfil["eventNotificationOn"] = notificationOn;
-                ownProfil["newFriendNotificationOn"] = notificationOn;
-                ownProfil["familiesDistance"] = value ? 50 : 0;
-                ownProfil["travelPlanNotification"] = notificationOn;
-              });
+                setState(() {
+                  ownProfil["notificationstatus"] = notificationOn;
+                  ownProfil["chatNotificationOn"] = notificationOn;
+                  ownProfil["eventNotificationOn"] = notificationOn;
+                  ownProfil["newFriendNotificationOn"] = notificationOn;
+                  ownProfil["familiesDistance"] = value ? 50 : 0;
+                  ownProfil["travelPlanNotification"] = notificationOn;
+                });
 
-              ProfilDatabase().updateProfil(
-                  "notificationstatus = '$notificationOn', "
-                      "chatNotificationOn = '$notificationOn', "
-                      "eventNotificationOn = '$notificationOn',"
-                      "newFriendNotificationOn = '$notificationOn',"
-                      "familiesDistance = ${value ? 50 : 0},"
-                      "travelPlanNotification = '$notificationOn'",
-                  "WHERE id = '$userId'");
-            })
-      ],
+                ProfilDatabase().updateProfil(
+                    "notificationstatus = '$notificationOn', "
+                        "chatNotificationOn = '$notificationOn', "
+                        "eventNotificationOn = '$notificationOn',"
+                        "newFriendNotificationOn = '$notificationOn',"
+                        "familiesDistance = ${value ? 50 : 0},"
+                        "travelPlanNotification = '$notificationOn'",
+                    "WHERE id = '$userId'");
+              })
+        ],
+      ),
     );
   }
 
   chatNotificationSetting() {
-    return Row(
-      children: [
-        const SizedBox(width: 20),
-        Text(
-            AppLocalizations.of(context)!.chatNotification,
-            style: const TextStyle(fontSize: 20)),
-        const Expanded(child: SizedBox(width: 20)),
-        Switch(
-            value: ownProfil["chatNotificationOn"] == 1 ? true : false,
-            onChanged: (value) {
-              var notificationOn = value == true ? 1 : 0;
+    return Container(
+      margin: const EdgeInsets.all(10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Text(
+                  AppLocalizations.of(context)!.chatNotification,
+                  style: const TextStyle(fontSize: 20)),
+              const Expanded(child: SizedBox(width: 20)),
+              Switch(
+                  value: ownProfil["chatNotificationOn"] == 1 ? true : false,
+                  onChanged: (value) {
+                    var notificationOn = value == true ? 1 : 0;
 
-              setState(() {
-                ownProfil["chatNotificationOn"] = notificationOn;
-              });
-              ProfilDatabase().updateProfil("chatNotificationOn = '$notificationOn'",
-                  "WHERE id = '${ownProfil["id"]}'");
-            })
-      ],
+                    setState(() {
+                      ownProfil["chatNotificationOn"] = notificationOn;
+                    });
+                    ProfilDatabase().updateProfil("chatNotificationOn = '$notificationOn'",
+                        "WHERE id = '${ownProfil["id"]}'");
+                  })
+            ],
+          ),
+          Text(AppLocalizations.of(context)!.chatNotificationHinweis)
+        ],
+      ),
     );
   }
 
   eventNotificationSetting() {
-    return Row(
-      children: [
-        const SizedBox(width: 20),
-        Text(
-            AppLocalizations.of(context)!.meetupNotification,
-            style: const TextStyle(fontSize: 20)),
-        const Expanded(child: SizedBox(width: 20)),
-        Switch(
-            value: ownProfil["eventNotificationOn"] == 1 ? true : false,
-            onChanged: (value) {
-              var notificationOn = value == true ? 1 : 0;
+    return Container(
+      margin: const EdgeInsets.all(10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Text(
+                  AppLocalizations.of(context)!.meetupNotification,
+                  style: const TextStyle(fontSize: 20)),
+              const Expanded(child: SizedBox(width: 20)),
+              Switch(
+                  value: ownProfil["eventNotificationOn"] == 1 ? true : false,
+                  onChanged: (value) {
+                    var notificationOn = value == true ? 1 : 0;
 
-              setState(() {
-                ownProfil["eventNotificationOn"] = notificationOn;
-              });
-              ProfilDatabase().updateProfil("eventNotificationOn = '$notificationOn'",
-                  "WHERE id = '${ownProfil["id"]}'");
-            })
-      ],
+                    setState(() {
+                      ownProfil["eventNotificationOn"] = notificationOn;
+                    });
+                    ProfilDatabase().updateProfil("eventNotificationOn = '$notificationOn'",
+                        "WHERE id = '${ownProfil["id"]}'");
+                  })
+            ],
+          ),
+          Text(AppLocalizations.of(context)!.meetupNotificationHinweis)
+        ],
+      ),
     );
   }
 
   newFriendNotificationSetting() {
-    return Row(
-      children: [
-        const SizedBox(width: 20),
-        Text(
-            AppLocalizations.of(context)!.friendNotification,
-            style: const TextStyle(fontSize: 20)),
-        const Expanded(child: SizedBox(width: 20)),
-        Switch(
-            value: ownProfil["newFriendNotificationOn"] == 1 ? true : false,
-            onChanged: (value) {
-              var notificationOn = value == true ? 1 : 0;
+    return Container(
+      margin: const EdgeInsets.all(10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Text(
+                  AppLocalizations.of(context)!.friendNotification,
+                  style: const TextStyle(fontSize: 20)),
+              const Expanded(child: SizedBox(width: 20)),
+              Switch(
+                  value: ownProfil["newFriendNotificationOn"] == 1 ? true : false,
+                  onChanged: (value) {
+                    var notificationOn = value == true ? 1 : 0;
 
-              setState(() {
-                ownProfil["newFriendNotificationOn"] = notificationOn;
-              });
-              ProfilDatabase().updateProfil(
-                  "newFriendNotificationOn = '$notificationOn'",
-                  "WHERE id = '${ownProfil["id"]}'");
-            })
-      ],
+                    setState(() {
+                      ownProfil["newFriendNotificationOn"] = notificationOn;
+                    });
+                    ProfilDatabase().updateProfil(
+                        "newFriendNotificationOn = '$notificationOn'",
+                        "WHERE id = '${ownProfil["id"]}'");
+                  })
+            ],
+          ),
+          Text(AppLocalizations.of(context)!.newFriendNotificationHinweis)
+        ],
+      ),
     );
   }
 
@@ -127,79 +153,90 @@ class _NotificationsOptionsPageState extends State<NotificationsOptionsPage> {
     double distance = ownProfil["familiesDistance"].toDouble() ?? 50.0;
     bool familieInRangeNotificationOn = distance > 0;
 
-    return Column(
-      children: [
-        Row(
-          children: [
-            const SizedBox(width: 20),
-            Text(
-                AppLocalizations.of(context)!.familieInRangeNotification,
-                style: const TextStyle(fontSize: 20)),
-            const Expanded(child: SizedBox(width: 20)),
-            Switch(
-                value: familieInRangeNotificationOn,
-                onChanged: (value) {
-                  var distance = value ? 50 : 0;
+    return Container(
+      margin: const EdgeInsets.all(10),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Text(
+                  AppLocalizations.of(context)!.familieInRangeNotification,
+                  style: const TextStyle(fontSize: 20)),
+              const Expanded(child: SizedBox(width: 20)),
+              Switch(
+                  value: familieInRangeNotificationOn,
+                  onChanged: (value) {
+                    var distance = value ? 50 : 0;
 
-                  setState(() {
-                    ownProfil["familiesDistance"] = distance;
-                  });
+                    setState(() {
+                      ownProfil["familiesDistance"] = distance;
+                    });
 
-                  ProfilDatabase().updateProfil(
-                      "familiesDistance = $distance",
-                      "WHERE id = '$userId'"
-                  );
-                }
-            )
-          ],
-        ),
-        if(familieInRangeNotificationOn) Text(
-            "${distance.round()} km ${AppLocalizations.of(context)!.umkreis}"
-        ),
-        if(familieInRangeNotificationOn) Slider(
-          value: distance,
-          min: 5,
-          max: 200,
-          divisions: 100,
-          label: '${distance.round()} km',
-          onChanged: (newDistance){
-            setState(() {
-              distance = newDistance;
-              ownProfil["familiesDistance"] = newDistance;
-            });
-          },
-          onChangeEnd: (newDistance){
-            ProfilDatabase().updateProfil(
-                "familiesDistance = $newDistance",
-                "WHERE id = '$userId'"
-            );
-          },
-        )
-      ],
+                    ProfilDatabase().updateProfil(
+                        "familiesDistance = $distance",
+                        "WHERE id = '$userId'"
+                    );
+                  }
+              ),
+            ],
+          ),
+          if(familieInRangeNotificationOn) Text(
+              "${distance.round()} km ${AppLocalizations.of(context)!.umkreis}"
+          ),
+          if(familieInRangeNotificationOn) Slider(
+            value: distance,
+            min: 5,
+            max: 200,
+            divisions: 100,
+            label: '${distance.round()} km',
+            onChanged: (newDistance){
+              setState(() {
+                distance = newDistance;
+                ownProfil["familiesDistance"] = newDistance;
+              });
+            },
+            onChangeEnd: (newDistance){
+              ProfilDatabase().updateProfil(
+                  "familiesDistance = $newDistance",
+                  "WHERE id = '$userId'"
+              );
+            },
+          ),
+          Text(AppLocalizations.of(context)!.familieAroundNotificationHinweis)
+        ],
+      ),
     );
   }
 
   friendsTravelPlanNotificationSetting(){
-    return Row(
-      children: [
-        const SizedBox(width: 20),
-        Text(
-            AppLocalizations.of(context)!.reiseplanungNotification,
-            style: const TextStyle(fontSize: 20)),
-        const Expanded(child: SizedBox(width: 20)),
-        Switch(
-            value: ownProfil["travelPlanNotification"] == 1 ? true : false,
-            onChanged: (value) {
-              var notificationOn = value == true ? 1 : 0;
+    return Container(
+      margin: const EdgeInsets.all(10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Text(
+                  AppLocalizations.of(context)!.reiseplanungNotification,
+                  style: const TextStyle(fontSize: 20)),
+              const Expanded(child: SizedBox(width: 20)),
+              Switch(
+                  value: ownProfil["travelPlanNotification"] == 1 ? true : false,
+                  onChanged: (value) {
+                    var notificationOn = value == true ? 1 : 0;
 
-              setState(() {
-                ownProfil["travelPlanNotification"] = notificationOn;
-              });
-              ProfilDatabase().updateProfil(
-                  "travelPlanNotification = '$notificationOn'",
-                  "WHERE id = '${ownProfil["id"]}'");
-            })
-      ],
+                    setState(() {
+                      ownProfil["travelPlanNotification"] = notificationOn;
+                    });
+                    ProfilDatabase().updateProfil(
+                        "travelPlanNotification = '$notificationOn'",
+                        "WHERE id = '${ownProfil["id"]}'");
+                  })
+            ],
+          ),
+          Text(AppLocalizations.of(context)!.travelPlanNotificationHinweis)
+        ],
+      ),
     );
   }
 
@@ -212,7 +249,7 @@ class _NotificationsOptionsPageState extends State<NotificationsOptionsPage> {
           CustomAppBar(
               title: AppLocalizations.of(context)!.benachrichtigungen
           ),
-      body: Column(
+      body: ListView(
         children: [
           const SizedBox(
             height: 20,
