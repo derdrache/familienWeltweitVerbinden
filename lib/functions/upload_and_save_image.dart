278b096e0483bc8cc544e3cd1886d44d1f3ad350
DevDrache <dominik.mast.11@gmail.com>
2024-10-05 10:38:52 +0300

    update flutter & plugins

--- lib/functions/upload_and_save_image.dart
+++ lib/functions/upload_and_save_image.dart
@@ -24,22 +24,30 @@ uploadAndSaveImage(context, typ, {folder = "",meetupCommunityData}) async{
 
     CroppedFile? croppedFile = await ImageCropper().cropImage(
       sourcePath: imageData["path"],
-      aspectRatioPresets: [
-        CropAspectRatioPreset.square,
-        CropAspectRatioPreset.ratio3x2,
-        CropAspectRatioPreset.original,
-        CropAspectRatioPreset.ratio4x3,
-        CropAspectRatioPreset.ratio16x9
-      ],
       uiSettings: [
         AndroidUiSettings(
             toolbarTitle: AppLocalizations.of(context)!.bildBearbeiten,
             toolbarColor: Theme.of(context).colorScheme.primary,
             toolbarWidgetColor: Colors.white,
             initAspectRatio: CropAspectRatioPreset.original,
-            lockAspectRatio: false),
+            lockAspectRatio: false,
+            aspectRatioPresets: [
+              CropAspectRatioPreset.square,
+              CropAspectRatioPreset.ratio3x2,
+              CropAspectRatioPreset.original,
+              CropAspectRatioPreset.ratio4x3,
+              CropAspectRatioPreset.ratio16x9
+            ],
+        ),
         IOSUiSettings(
           title: 'Cropper',
+          aspectRatioPresets: [
+            CropAspectRatioPreset.square,
+            CropAspectRatioPreset.ratio3x2,
+            CropAspectRatioPreset.original,
+            CropAspectRatioPreset.ratio4x3,
+            CropAspectRatioPreset.ratio16x9
+          ],
         ),
         WebUiSettings(
           context: context,
