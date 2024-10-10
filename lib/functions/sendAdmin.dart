3ea1c43584c460f48d2683b5028895e54e391cb9
DevDrache <dominik.mast.11@gmail.com>
2024-10-10 09:16:41 +0300

    add more report information

--- lib/functions/sendAdmin.dart
+++ lib/functions/sendAdmin.dart
@@ -2,16 +2,19 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 import '../auth/secrets.dart';
+import '../services/database.dart';
 
 addAdminMessage(title, message, user) {
   var url = Uri.parse(databaseUrl + databasePathNewAdminMessage);
   http.post(url, body: json.encode({"message": message, "user": user}));
+
+  var userName = getProfilFromHive(profilId: user, getNameOnly: true);
 
   url = Uri.parse("${databaseUrl}services/sendEmail.php");
   http.post(url,
       body: json.encode({
         "to": adminEmail,
         "title": title,
-        "inhalt": message
+        "inhalt": message + "\n\n von: $userName"
       }));
 }
